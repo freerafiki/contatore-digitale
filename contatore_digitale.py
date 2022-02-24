1fafed75b54f7a5d87c906847ff6dabec736abd5
freerafiki <bagigi@disroot.org>
2022-02-24 22:32:51 +0100

    small graph changes

--- contatore_digitale.py
+++ contatore_digitale.py
@@ -71,9 +71,8 @@ else:
 
 st.markdown("## Grafici")
 st.markdown("""
-Questa sezione è dedicata ai grafici.
-Lo scopo è visualizzare e dare un'idea della tendenza.
-Scegliendo dal menu la zona il grafico si aggiorna automaticamente.
+Il primo grafico mostra il numero di residenti (in totale, senza divisioni per fasce d'età) della zona selezionata.
+Scegliendo dal menu (in alto a sinistra) la zona il grafico si aggiorna automaticamente.
 """)
 
 # aliceblue, antiquewhite, aqua, aquamarine, azure, beige, bisque, black, blanchedalmond, blue, blueviolet, brown, burlywood, cadetblue, chartreuse, chocolate, coral, cornflowerblue, cornsilk, crimson, cyan, darkblue, darkcyan, darkgoldenrod, darkgray, darkgrey, darkgreen, darkkhaki, darkmagenta, darkolivegreen, darkorange, darkorchid, darkred, darksalmon, darkseagreen, darkslateblue, darkslategray, darkslategrey, darkturquoise, darkviolet, deeppink, deepskyblue, dimgray, dimgrey, dodgerblue, firebrick, floralwhite, forestgreen, fuchsia, gainsboro, ghostwhite, gold, goldenrod, gray, grey, green, greenyellow, honeydew, hotpink, indianred, indigo, ivory, khaki, lavender, lavenderblush, lawngreen, lemonchiffon, lightblue, lightcoral, lightcyan, lightgoldenrodyellow, lightgray, lightgrey, lightgreen, lightpink, lightsalmon, lightseagreen, lightskyblue, lightslategray, lightslategrey, lightsteelblue, lightyellow, lime, limegreen, linen, magenta, maroon, mediumaquamarine, mediumblue, mediumorchid, mediumpurple, mediumseagreen, mediumslateblue, mediumspringgreen, mediumturquoise, mediumvioletred, midnightblue, mintcream, mistyrose, moccasin, navajowhite, navy, oldlace, olive, olivedrab, orange, orangered, orchid, palegoldenrod, palegreen, paleturquoise, palevioletred, papayawhip, peachpuff, peru, pink, plum, powderblue, purple, red, rosybrown, royalblue, rebeccapurple, saddlebrown, salmon, sandybrown, seagreen, seashell, sienna, silver, skyblue, slateblue, slategray, slategrey, snow, springgreen, steelblue, tan, teal, thistle, tomato, turquoise, violet, wheat, white, whitesmoke, yellow, yellowgreen
@@ -88,7 +87,7 @@ fig.add_trace(go.Scatter(
     y=y_vals,
     hoverinfo='x+y',
     fill='tozeroy',
-    line=dict(width=0.5, color='turquoise')
+    line=dict(width=1.5, color='turquoise')
     #stackgroup='one', # define stack group
     #groupnorm='fraction' # sets the normalization for the sum of the stackgroup
 ))
